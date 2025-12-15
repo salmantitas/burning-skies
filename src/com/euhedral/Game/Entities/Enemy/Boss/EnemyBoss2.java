71e349323384a869c1c2f795d6c660de75cc80fd
Salman Titas <salman.mehedy@gmail.com>
2025-12-14 23:41:47 -0800

    Enemy Boss 2

--- src/com/euhedral/Game/Entities/Enemy/Boss/EnemyBoss2.java
+++ src/com/euhedral/Game/Entities/Enemy/Boss/EnemyBoss2.java
@@ -3,15 +3,36 @@ package com.euhedral.Game.Entities.Enemy.Boss;
 import com.euhedral.Engine.Engine;
 import com.euhedral.Engine.Utility;
 import com.euhedral.Game.Difficulty;
+import com.euhedral.Game.EntityHandler;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public class EnemyBoss2 extends EnemyBoss1 {
+
+    boolean firstMove = true;
+    double destinationX, destinationY;
+
+    int minY,
+    maxY;
 
     public EnemyBoss2(double x, double y, int levelHeight) {
         super(x,y, levelHeight);
         height = 128;
         width = height;
+
+        minX = Utility.percWidth(10);
+        maxX = Utility.percWidth(90) - width;
+        minY = Utility.percHeight(35);
+        maxY = Utility.percHeight(100) - height;
+
+        debug = false;
+        tracking = true;
+
+        moveLeft = false;
+        moveRight = false;
+        moveUp = false;
+        moveDown = true;
 //        pos.x = x - width/2;
 //        color = Color.orange;
 //
@@ -27,18 +48,48 @@ public class EnemyBoss2 extends EnemyBoss1 {
         setImage(textureHandler.enemyBoss[1]);
     }
 
+    @Override
+    public void update() {
+        super.update();
+        if (state == STATE_ACTIVE && shootTimer <= 50) {
+            updateDestination();
+        }
+    }
+
 //    @Override
 //    public void update() {
 //        super.update();
 //        move();
 //    }
 
-//    @Override
-//    public void render(Graphics g) {
-//        super.render(g);
-//        g.setColor(color);
-//        g.fillRect((int) pos.x, (int) pos.y, width, height);
-//    }
+    @Override
+    public void render(Graphics g) {
+        g2d = (Graphics2D) g;
+        AffineTransform currentTransform = g2d.getTransform();
+
+        if (moveLeft) {
+            g2d.rotate(Math.toRadians(90), pos.intX() + width/2, pos.intY() + height/2);
+        }
+
+        if (moveRight) {
+            g2d.rotate(Math.toRadians(-90), pos.intX() + width/2, pos.intY() + height/2);
+        }
+
+        if (moveUp) {
+            g2d.rotate(Math.toRadians(180), pos.intX() + width/2, pos.intY() + height/2);
+        }
+
+        super.render(g);
+
+        g2d.setTransform(currentTransform);
+
+        if (isActive() && debug) {
+            g.setColor(Color.RED);
+            g.drawRect(minX, minY, maxX - minX, maxY - minY);
+        }
+
+
+    }
 
 
 //    @Override
@@ -68,24 +119,70 @@ public class EnemyBoss2 extends EnemyBoss1 {
 //        }
 //        shootTimer = (int) (factor * shootTimerDefault / Difficulty.getEnemyFireRateMult());
 //    }
-//
-//    public void moveInScreen() {
+
+    @Override
+    public void moveInScreen() {
 //        if (distToCover > 0) {
 //            distToCover--;
 //            pos.y += velY;
+//        } else {
+
+            velY = velX;
+
+            if (moveDown) {
+                pos.y += velY;
+            }
+
+            if (moveUp)
+                pos.y -= velY;
+
+            if (moveLeft)
+                pos.x -= velX;
+
+            if (moveRight)
+                pos.x += velX;
 //        }
-//        else {
-//            if (!moveLeft)
-//                pos.x += velX;
-//            else
-//                pos.x -= velX;
-//        }
-//
-//        if (pos.x < min)
-//            moveLeft = false;
-//        if (pos.x > max)
-//            moveLeft = true;
-//    }
+
+        boolean bottomCentre = pos.y > maxY && pos.x >minX && pos.x < maxX;
+        boolean bottomLeft = pos.x < minX && pos.y > maxY;
+        boolean bottomRight = pos.x > maxX && pos.y > maxY;
+        boolean topLeft = pos.x < minX && pos.y < minY;
+        boolean topRight = pos.x  > maxX && pos.y < minY;
+
+        if (firstMove && bottomCentre) {
+            moveRight = true;
+            moveDown = false;
+            firstMove = false;
+        }
+
+        if (bottomLeft) {
+            moveLeft = false;
+            moveRight = true;
+            moveDown = false;
+            moveUp = false;
+        }
+
+        if (bottomRight) {
+            moveLeft = false;
+            moveRight = false;
+            moveDown = false;
+            moveUp = true;
+        }
+
+        if (topLeft) {
+            moveUp = false;
+            moveDown = true;
+            moveLeft = false;
+            moveRight = false;
+        }
+
+        if (topRight) {
+            moveUp = false;
+            moveDown = false;
+            moveLeft = true;
+            moveRight = false;
+        }
+    }
 
     // Private Methods
 
@@ -113,4 +210,14 @@ public class EnemyBoss2 extends EnemyBoss1 {
 //            currentGun = 0;
 //        }
 //    }
+
+    @Override
+    public double getBulletAngle() {
+        return calculateAngle(getTurretX(), getTurretY(), destinationX, destinationY); // stub
+    }
+
+    private void updateDestination() {
+        destinationX = EntityHandler.playerPositon.x;
+        destinationY = EntityHandler.playerPositon.y;
+    }
 }
