e8750210c6d6cccd1318e62e55db838303cd4cf5
Salman Titas <salman.mehedy@gmail.com>
2025-12-22 17:33:26 -0800

    Tracker improvements

--- src/com/euhedral/Game/Entities/Enemy/Component/Tracker.java
+++ src/com/euhedral/Game/Entities/Enemy/Component/Tracker.java
@@ -1,9 +1,14 @@
 package com.euhedral.Game.Entities.Enemy.Component;
 
+import com.euhedral.Engine.Utility;
 import com.euhedral.Game.EntityHandler;
 
 public class Tracker {
     public double destinationX, destinationY;
+
+    public Tracker() {
+        updateDestination();
+    }
 
     public void updateDestination() {
         destinationX = EntityHandler.playerPositon.x;
